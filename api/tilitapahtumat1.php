7e8c543d8dc50ea73dec1763eebfac8937a400f4
Janne Prokkola <jprokkola@gmail.com>
2018-10-15 15:33:51 +0300

    lisätty tietokanta ja PDF

--- api/tilitapahtumat1.php
+++ api/tilitapahtumat1.php
@@ -1,6 +1,6 @@
 <?php
 	require "connection.php";
-	$sql = "SELECT * FROM Tilitapahtumat where idTili = 1";
+	$sql = "SELECT * FROM Tilitapahtumat where idTili = 2";
 
 	$resultObject = $db->query($sql);
 
